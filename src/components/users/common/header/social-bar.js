0afea1f5971f290243f6d8a226b725a2fb825d2c
cener78 <94464442+cener78@users.noreply.github.com>
2022-05-16 23:01:52 +0200

    First

---
+++ src/components/users/common/header/social-bar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  RiFacebookFill,
+  RiTwitterFill,
+  RiInstagramFill,
+  RiWhatsappFill,
+  RiLinkedinFill,
+} from "react-icons/ri";
+const SocialBar = () => {
+  return (
+    <ul className="social-bar">
+      <li>
+        <a href="#" target="_blank">
+          <RiFacebookFill />
+        </a>
+      </li>
+      <li>
+        <a href="#" target="_blank">
+          <RiTwitterFill />
+        </a>
+      </li>
+      <li>
+        <a href="#" target="_blank">
+          <RiInstagramFill />
+        </a>
+      </li>
+      <li>
+        <a href="#" target="_blank">
+          <RiWhatsappFill />
+        </a>
+      </li>
+      <li>
+        <a href="#" target="_blank">
+          <RiLinkedinFill />
+        </a>
+      </li>
+    </ul>
+  );
+};
+export default SocialBar;
